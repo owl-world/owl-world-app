b9f724a4b33e470aac6d8e9b448bffee838eaca9
yunyeop <yykim000922@gmail.com>
2023-02-23 04:05:20 +0900

    feat : 절대경로 설정 (@/[path])

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@': './src',
+          '@providers': './src/providers',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
