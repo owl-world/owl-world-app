aa0e58b944ba6e42a0b5b9c9edaae3259f3cc6b4
yunyeop <yykim000922@gmail.com>
2023-02-26 08:52:00 +0900

    refactor

--- src/screens/Policy/PolicyPresenter.tsx
+++ src/screens/Policy/PolicyPresenter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Header } from '@/components/Header';
 import { SearchInput } from '@/components/Input';
@@ -14,6 +14,8 @@ type Props = {
   onPressUniversity: (universityId: number, code: string) => void;
 };
 
+const Spacer = () => <SplitRow height={10} />;
+
 export const PolicyPresenter = ({ universities, onChange, onPressUniversity }: Props) => {
   const { top } = useSafeAreaInsets();
 
@@ -25,26 +27,25 @@ export const PolicyPresenter = ({ universities, onChange, onPressUniversity }: P
 
       <SplitRow height={30} />
 
-      <ScrollView style={styles.mainContainer}>
-        <SearchInput placeholder="검색어를 입력해주세요." onChangeText={text => onChange(text)} />
-
-        <SplitRow height={10} />
-
-        {universities &&
-          universities.map(university => {
-            return (
-              <React.Fragment key={university.name}>
-                <TouchableOpacity
-                  style={styles.universityContainer}
-                  onPress={() => onPressUniversity(university.id, university.code)}
-                >
-                  <UniversityRow university={university} />
-                </TouchableOpacity>
-                <SplitRow height={10} />
-              </React.Fragment>
-            );
-          })}
-      </ScrollView>
+      <FlatList
+        keyExtractor={item => item.id.toString()}
+        contentContainerStyle={styles.mainContainer}
+        data={universities}
+        ListHeaderComponent={
+          <React.Fragment>
+            <SearchInput placeholder="검색어를 입력해주세요." onChangeText={text => onChange(text)} />
+            <SplitRow height={10} />
+          </React.Fragment>
+        }
+        ItemSeparatorComponent={Spacer}
+        renderItem={({ item }) => (
+          <React.Fragment key={item.name}>
+            <TouchableOpacity style={styles.universityContainer} onPress={() => onPressUniversity(item.id, item.code)}>
+              <UniversityRow university={item} />
+            </TouchableOpacity>
+          </React.Fragment>
+        )}
+      />
     </View>
   );
 };
