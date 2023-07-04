712dc3a764d549bff70e5024881fd614938a68c3
yunyeop <yykim000922@gmail.com>
2023-07-04 17:22:03 +0900

    fix : 로그인 페이지 input 간격조정

--- src/components/Input/SignInput.tsx
+++ src/components/Input/SignInput.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { StyleSheet, TextInput, TextInputProps } from 'react-native';
+import { StyleSheet, TextInputProps, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { height } from '@/utils/globalStyles';
 
 export const SignInput = ({ ...rest }: TextInputProps) => {
   return (
-    <TextInput placeholderTextColor={'#7D7D7D'} autoCapitalize="none" {...rest} style={[styles.input, rest.style]} />
+    <View style={styles.container}>
+      <TextInput
+        placeholderTextColor={'#7D7D7D'}
+        autoCapitalize="none"
+        {...rest}
+        style={[styles.input, rest.style]}
+        multiline={false}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    fontSize: 13,
-    fontWeight: '400',
-    lineHeight: 16,
+  container: {
     borderRadius: 10,
-    paddingVertical: 17,
     paddingHorizontal: 10,
+    paddingVertical: height * 14,
     backgroundColor: '#FFFDF7',
     shadowColor: 'rgb(0,0,0)',
     shadowOpacity: 0.1,
@@ -23,5 +30,11 @@ const styles = StyleSheet.create({
       height: 4,
     },
     elevation: 4,
+  },
+  input: {
+    fontSize: 13,
+    fontWeight: '400',
+    lineHeight: 16,
+    padding: 0,
   },
 });
